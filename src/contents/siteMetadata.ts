7a06bea81e0d65f429f0656109594a8651c65e5c
Brian Shen <shenhaoxiang1990@gmail.com>
2024-05-01 09:37:38 -0300

    feat: update status

--- src/contents/siteMetadata.ts
+++ src/contents/siteMetadata.ts
@@ -77,7 +77,7 @@ export const author = {
   locationIcon: '🇨🇦',
   position: 'Full Stack Web Developer',
   calMeetingUrl: '',
-  status: 'Working remotely around the world',
+  status: 'Restart in Fredericton',
   workingOn: `I am learning and practicing the latest AI technology.`,
   workTogether: `I'm open for new opportunities, feel free to email me to see how can we collaborate.`,
   wakatime: 'https://wakatime.com/@BrianShen',
