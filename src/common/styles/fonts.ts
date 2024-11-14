3792d86fb1e428820a14b1c85f708227f11c62d9
mpc <mpengchong@gmail.com>
2024-11-14 12:08:00 -0600

    Update fonts.ts

--- src/common/styles/fonts.ts
+++ src/common/styles/fonts.ts
@@ -1,21 +1,7 @@
-import { Fira_Code, Plus_Jakarta_Sans, Sora } from 'next/font/google';
-
-export const jakartaSans = Plus_Jakarta_Sans({
-  variable: '--jakartaSans-font',
-  subsets: ['latin'],
-  display: 'fallback',
-  weight: ['400', '500', '600', '700', '800'],
-});
+import { Fira_Code } from 'next/font/google';
 
 export const firaCode = Fira_Code({
   variable: '--font-fira-code',
   subsets: ['latin'],
-  display: 'swap',
-});
-
-export const soraSans = Sora({
-  variable: '--soraSans-font',
-  subsets: ['latin'],
-  display: 'fallback',
-  weight: ['300', '400', '500', '600', '700', '800'],
+  display: 'swap',  // This will fallback to a system font if the font fails to load
 });
