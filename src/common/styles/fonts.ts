98e76f1245b9cb536011adc51d5a505ee0127424
mpc <mpengchong@gmail.com>
2024-11-14 11:43:53 -0600

    Update fonts.ts

--- src/common/styles/fonts.ts
+++ src/common/styles/fonts.ts
@@ -1,29 +1,21 @@
-// import { Fira_Code, Plus_Jakarta_Sans, Sora } from 'next/font/google';
-
-// export const jakartaSans = Plus_Jakarta_Sans({
-//   variable: '--jakartaSans-font',
-//   subsets: ['latin'],
-//   display: 'fallback',
-//   weight: ['400', '500', '600', '700', '800'],
-// });
-
-// export const firaCode = Fira_Code({
-//   variable: '--font-fira-code',
-//   subsets: ['latin'],
-//   display: 'swap',
-// });
-
-// export const soraSans = Sora({
-//   variable: '--soraSans-font',
-//   subsets: ['latin'],
-//   display: 'fallback',
-//   weight: ['300', '400', '500', '600', '700', '800'],
-// });
-import { Plus_Jakarta_Sans } from 'next/font/google';
+import { Fira_Code, Plus_Jakarta_Sans, Sora } from 'next/font/google';
 
 export const jakartaSans = Plus_Jakarta_Sans({
+  variable: '--jakartaSans-font',
+  subsets: ['latin'],
+  display: 'fallback',
+  weight: ['400', '500', '600', '700', '800'],
+});
+
+export const firaCode = Fira_Code({
+  variable: '--font-fira-code',
   subsets: ['latin'],
   display: 'swap',
-  weight: ['400', '500', '600', '700', '800'],
-  fallback: ['Arial', 'sans-serif'], // Use local fallbacks
+});
+
+export const soraSans = Sora({
+  variable: '--soraSans-font',
+  subsets: ['latin'],
+  display: 'fallback',
+  weight: ['300', '400', '500', '600', '700', '800'],
 });
