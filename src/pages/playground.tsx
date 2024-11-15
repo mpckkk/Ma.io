8a6a9a425bcee38a9b069b9d47e9eb957bfa9b6a
mpc <mpengchong@gmail.com>
2024-11-15 16:00:28 -0600

    Update playground.tsx

--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -3,7 +3,6 @@ import { NextSeo } from 'next-seo';
 import React from 'react';
 
 import Container from '@/common/components/elements/Container';
-import { siteMetadata } from '@/contents/siteMetadata';
 import Playground from '@/modules/playground';
 
 const PAGE_TITLE = 'JavaScript Playground';
@@ -11,7 +10,7 @@ const PAGE_TITLE = 'JavaScript Playground';
 const playground: NextPage = () => {
   return (
     <>
-      <NextSeo title={`${PAGE_TITLE} - ${siteMetadata.author}`} />
+      <NextSeo title={`${PAGE_TITLE} - Ryan Aulia`} />
       <Container className='!mt-0 pt-20 md:pt-0' data-aos='fade-up'>
         <Playground id='playground' isHeading />
       </Container>
