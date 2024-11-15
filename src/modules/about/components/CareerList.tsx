c73f296de0ffb4b1169d7acc060905041d50256e
mpc <mpengchong@gmail.com>
2024-11-15 16:59:34 -0600

    Update CareerList.tsx

--- src/modules/about/components/CareerList.tsx
+++ src/modules/about/components/CareerList.tsx
@@ -1,25 +1,12 @@
-import { HiOutlineBriefcase as CareerIcon } from 'react-icons/hi';
-
-import SectionHeading from '@/common/components/elements/SectionHeading';
-import SectionSubHeading from '@/common/components/elements/SectionSubHeading';
-import { CareerProps } from '@/common/types/careers';
+import { CAREERS } from '@/common/constant/careers';
 
 import CareerCard from './CareerCard';
 
-const CareerList = ({ careerList }: { careerList: CareerProps[] }) => {
+const CareerList = () => {
   return (
     <section className='space-y-6'>
-      <div className='space-y-2'>
-        <SectionHeading title='Career' icon={<CareerIcon className='mr-1' />} />
-        <SectionSubHeading>
-          <p className='dark:text-neutral-400'>
-            My professional career journey.
-          </p>
-        </SectionSubHeading>
-      </div>
-
-      <div className='grid md:grid-cols-2 gap-4'>
-        {careerList?.map((career, index) => (
+      <div className='grid gap-3 '>
+        {CAREERS?.map((career, index) => (
           <CareerCard key={index} {...career} />
         ))}
       </div>
