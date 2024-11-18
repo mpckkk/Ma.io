07e98c42906b2e2eff1449f977ea59da400b9f38
mpc <mpengchong@gmail.com>
2024-11-17 21:43:24 -0600

    Update Copyright.tsx

--- src/common/components/layouts/partials/Copyright.tsx
+++ src/common/components/layouts/partials/Copyright.tsx
@@ -3,9 +3,7 @@ import { siteMetadata } from '@/contents/siteMetadata';
 const Copyright = () => {
   return (
     <div className='flex items-center gap-1 text-sm py-1 px-3 text-neutral-600 dark:text-neutral-600 font-sora'>
-      <span>{new Date().getFullYear()}</span>
-      <span>© All rights reserved.</span>
-      <span>{siteMetadata.siteShortTitle}</span>
+      <span>{new Date().getFullYear()} © All rights reserved.</span>
     </div>
   );
 };
