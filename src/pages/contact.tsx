9a3e9adbf6392e046f4805adf3e0a6bc9063388e
mpc <mpengchong@gmail.com>
2024-11-17 22:49:21 -0600

    Update contact.tsx

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -9,7 +9,7 @@ import { siteMetadata } from '../contents/siteMetadata';
 
 const PAGE_TITLE = 'Contact';
 const PAGE_DESCRIPTION =
-  "Feel free to get in touch and let's have a discussion about how we can work together.";
+  "Let’s connect and explore how we can collaborate effectively to achieve impactful results. Feel free to reach out to discuss potential opportunities or projects.";
 
 const ContactPage: NextPage = () => {
   return (
