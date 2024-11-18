df8adff278b2da8a42807a03d93bf2414cfdf400
mpc <mpengchong@gmail.com>
2024-11-17 21:33:32 -0600

    Update Copyright.tsx

--- src/common/components/layouts/partials/Copyright.tsx
+++ src/common/components/layouts/partials/Copyright.tsx
@@ -4,10 +4,7 @@ const Copyright = () => {
   return (
     <div className='flex items-center gap-1 text-sm py-1 px-3 text-neutral-600 dark:text-neutral-600 font-sora'>
       <span>{new Date().getFullYear()}</span>
-      <span>©</span>
-      <span>by</span>
-      <a href={socialAccounts.github} target='_blank' rel='noopener noreferrer'>
-        <span className='hover:dark:text-neutral-400 cursor-pointer'>
+      <span>© All rights reserve.</span>
           {siteMetadata.siteShortTitle}
         </span>
       </a>
