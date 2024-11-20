90ef693d3dd46740311797a1cf41b312da46a0aa
mpc <mpengchong@gmail.com>
2024-11-20 08:15:32 -0600

    Update Sidebar.tsx

--- src/common/components/layouts/partials/Sidebar.tsx
+++ src/common/components/layouts/partials/Sidebar.tsx
@@ -1,9 +1,56 @@
+// import { useEffect, useState } from 'react';
+
+// import useIsMobile from '@/common/hooks/useIsMobile';
+
+// import Copyright from './Copyright';
+// import Breakline from '../../elements/Breakline';
+// import Navigation from '../../sidebar/Navigation';
+// import Profile from '../../sidebar/Profile';
+
+// const Sidebar = () => {
+//   const isMobile = useIsMobile();
+//   const [isScrolled, setIsScrolled] = useState(false);
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       const scrollTop =
+//         window.pageYOffset || document.documentElement.scrollTop;
+//       setIsScrolled(scrollTop > 0);
+//     };
+
+//     window.addEventListener('scroll', handleScroll);
+
+//     return () => {
+//       window.removeEventListener('scroll', handleScroll);
+//     };
+//   }, []);
+
+//   return (
+//     <div
+//       id='sidebar'
+//       className='sticky transition-all duration-300 top-0 z-10 flex flex-col lg:py-8'
+//     >
+//       <Profile isScrolled={isScrolled} />
+//       {!isMobile && (
+//         <>
+//           <Breakline />
+//           <Navigation />
+//           <Breakline />
+//           <Copyright />
+//         </>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Sidebar;
 import { useEffect, useState } from 'react';
 
 import useIsMobile from '@/common/hooks/useIsMobile';
 
-import Copyright from './Copyright';
 import Breakline from '../../elements/Breakline';
+import SearchBox from '../../elements/SearchBox';
+import ThemeSwitcher from '../../elements/ThemeSwitcher';
 import Navigation from '../../sidebar/Navigation';
 import Profile from '../../sidebar/Profile';
 
@@ -28,16 +75,24 @@ const Sidebar = () => {
   return (
     <div
       id='sidebar'
-      className='sticky transition-all duration-300 top-0 z-10 flex flex-col lg:py-8'
+      // className='flex flex-col space-y-6 transition-all duration-300 lg:py-8'
+      className='sticky top-0 z-10 flex flex-col space-y-6 transition-all duration-300 lg:py-6'
     >
       <Profile isScrolled={isScrolled} />
       {!isMobile && (
-        <>
-          <Breakline />
+        <div className='space-y-3'>
+          <div className='pb-1'>
+            <SearchBox />
+          </div>
           <Navigation />
-          <Breakline />
-          <Copyright />
-        </>
+          <Breakline className='mx-1' />
+          <div className='space-y-2.5 px-1'>
+            <div className='px-3'>
+              <span className='text-sm text-neutral-600'>Theme</span>
+            </div>
+            <ThemeSwitcher />
+          </div>
+        </div>
       )}
     </div>
   );
