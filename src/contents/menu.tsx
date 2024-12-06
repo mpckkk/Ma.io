93a983a61d8f257fd429d108fffad0917a683397
mpc <mpengchong@gmail.com>
2024-12-05 20:02:10 -0600

    Update menu.tsx

--- src/contents/menu.tsx
+++ src/contents/menu.tsx
@@ -24,7 +24,7 @@ import { SiJavascript } from 'react-icons/si';
 import { MenuItemProps } from '@/common/types/menu';
 import { siteMetadata, socialAccounts } from '@/contents/siteMetadata';
 
-const iconSize = 30;
+const iconSize = 20;
 
 export const MENU_ITEMS: MenuItemProps[] = [
   {
