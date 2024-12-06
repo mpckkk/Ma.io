eaa43c3299641c41bf1bcccb7281b2cba21f7ea2
mpc <mpengchong@gmail.com>
2024-12-05 19:35:12 -0600

    Update menu.tsx

--- src/contents/menu.tsx
+++ src/contents/menu.tsx
@@ -1,3 +1,127 @@
+import { SiGooglescholar } from 'react-icons/si';
+import { BiRocket as ContactIcon } from 'react-icons/bi';
+import {
+  BsEnvelopeAtFill as EmailIcon,
+  BsGithub as GithubIcon,
+  BsInstagram as InstagramIcon,
+  BsLinkedin as LinkedinIcon,
+  BsTwitter as TwitterIcon,
+} from 'react-icons/bs';
+import {
+  FiBookOpen as LearnIcon,
+  FiCode as SnippetsIcon,
+  FiCoffee as ProjectIcon,
+  FiCpu as DashboardIcon,
+  FiLayers as WeeklyIcon,
+  FiPieChart as AnalyticsIcon,
+  FiPocket as HomeIcon,
+  FiRss as BlogIcon,
+  FiUser as ProfileIcon,
+} from 'react-icons/fi';
+// import { PiChatCircleDotsBold as ChatIcon } from 'react-icons/pi';
+import { SiJavascript } from 'react-icons/si';
+
+import { MenuItemProps } from '@/common/types/menu';
+import { siteMetadata, socialAccounts } from '@/contents/siteMetadata';
+
+const iconSize = 20;
+
+export const MENU_ITEMS: MenuItemProps[] = [
+  {
+    title: 'Home',
+    href: '/',
+    icon: <HomeIcon size={iconSize} />,
+    isShow: true,
+    isExternal: false,
+    eventName: 'Pages: Home',
+    type: 'Pages',
+  },
+  {
+    title: 'Dashboard',
+    href: '/dashboard',
+    icon: <DashboardIcon size={iconSize} />,
+    isShow: true,
+    isExternal: false,
+    eventName: 'Pages: Dashboard',
+    type: 'Pages',
+  },
+  {
+    title: 'Projects',
+    href: '/projects',
+    icon: <ProjectIcon size={iconSize} />,
+    isShow: false,
+    isExternal: false,
+    eventName: 'Pages: Projects',
+    type: 'Pages',
+  },
+  {
+    title: 'Blog',
+    href: '/blog',
+    icon: <BlogIcon size={iconSize} />,
+    isShow: true,
+    isExternal: false,
+    eventName: 'Pages: Blog',
+    type: 'Pages',
+  },
+  {
+    title: 'Weekly',
+    href: '/weekly',
+    icon: <WeeklyIcon size={iconSize} />,
+    isShow: false,
+    isExternal: false,
+    eventName: 'Pages: Weekly',
+    type: 'Pages',
+  },
+  {
+    title: 'Snippets',
+    href: '/snippets',
+    icon: <SnippetsIcon size={iconSize} />,
+    isShow: true,
+    isExternal: false,
+    eventName: 'Pages: Snippets',
+    type: 'Pages',
+  },
+  {
+    title: 'Learn',
+    href: '/learn',
+    icon: <LearnIcon size={iconSize} />,
+    isShow: true,
+    isExternal: false,
+    eventName: 'Pages: Learn',
+    type: 'Pages',
+  },
+  {
+    title: 'About',
+    href: '/about',
+    icon: <ProfileIcon size={iconSize} />,
+    isShow: true,
+    isExternal: false,
+    eventName: 'Pages: About',
+    type: 'Pages',
+  },
+  {
+    title: 'Contact',
+    href: '/contact',
+    icon: <ContactIcon size={iconSize} />,
+    isShow: true,
+    isExternal: false,
+    eventName: 'Pages: Contact',
+    type: 'Pages',
+  },
+];
+
+export const MENU_APPS: MenuItemProps[] = [
+  {
+    title: 'JS Playground',
+    href: '/playground',
+    icon: <SiJavascript size={iconSize} />,
+    isShow: true,
+    isExternal: false,
+    eventName: 'Pages: Playground',
+    type: 'Pages',
+  },
+];
+
 export const SOCIAL_MEDIA: MenuItemProps[] = [
   {
     title: 'Email',
@@ -7,6 +131,17 @@ export const SOCIAL_MEDIA: MenuItemProps[] = [
     isExternal: true,
     eventName: 'Contact: Email',
     className: '!bg-green-600 border border dark:border-neutral-700',
+    type: 'Link',
+  },
+
+  {
+    title: 'Linkedin',
+    href: socialAccounts.linkedin,
+    icon: <LinkedinIcon size={iconSize} />,
+    isShow: true,
+    isExternal: true,
+    eventName: 'Social: Linkedin',
+    className: '!bg-blue-500 border border dark:border-neutral-700',
     type: 'Link',
   },
   {
@@ -30,6 +165,16 @@ export const SOCIAL_MEDIA: MenuItemProps[] = [
     type: 'Link',
   },
   {
+  title: 'Google Scholar',
+  href: socialAccounts.googlescholar, // Replace with your ID
+  icon: <SiGooglescholar size={iconSize} />,
+  isShow: true,
+  isExternal: true,
+  eventName: 'Social: Google Scholar',
+  className: '!bg-blue-700 border border dark:border-neutral-700',
+  type: 'Link',
+  },
+  {
     title: 'Github',
     href: socialAccounts.github,
     icon: <GithubIcon size={iconSize} />,
@@ -39,24 +184,18 @@ export const SOCIAL_MEDIA: MenuItemProps[] = [
     className: '!bg-black border border dark:border-neutral-700',
     type: 'Link',
   },
+].filter((item) => {
+  return !!item.href;
+});
+
+export const EXTERNAL_LINKS: MenuItemProps[] = [
   {
-    title: 'Google Scholar',
-    href: socialAccounts.googlescholar, // Replace with your ID
-    icon: <SiGooglescholar size={iconSize} />,
-    isShow: true,
+    title: 'Analytics',
+    href: siteMetadata.analytics.analyticsURL,
+    icon: <AnalyticsIcon size={iconSize} />,
+    isShow: !!siteMetadata.analytics.analyticsURL,
     isExternal: true,
-    eventName: 'Social: Google Scholar',
-    className: '!bg-blue-700 border border dark:border-neutral-700',
+    eventName: 'External Link: Analytics',
     type: 'Link',
   },
-  {
-    title: 'Linkedin',
-    href: socialAccounts.linkedin,
-    icon: <LinkedinIcon size={iconSize} />,
-    isShow: true,
-    isExternal: true,
-    eventName: 'Social: Linkedin',
-    className: '!bg-blue-500 border border dark:border-neutral-700',
-    type: 'Link',
-  },
-].filter((item) => !!item.href);
+];
