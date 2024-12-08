2ffa95fc435b3360fe11272c7b267bcc6098b4ff
mpc <mpengchong@gmail.com>
2024-12-08 07:47:12 -0600

    Update CareerCard.tsx

--- src/modules/about/components/CareerCard.tsx
+++ src/modules/about/components/CareerCard.tsx
@@ -1,106 +1,67 @@
-// import { differenceInMonths, differenceInYears, format } from 'date-fns';
-// import { BsBuildings as CompanyIcon } from 'react-icons/bs';
+import { differenceInMonths, differenceInYears, format } from 'date-fns';
+import { BsBuildings as CompanyIcon } from 'react-icons/bs';
 
-// import Card from '@/common/components/elements/Card';
-// import Image from '@/common/components/elements/Image';
-// import { CareerProps } from '@/common/types/careers';
-
-// const CareerCard = ({
-//   position,
-//   company,
-//   logo,
-//   location,
-//   start_date,
-//   end_date,
-//   link,
-// }: CareerProps) => {
-//   const startDate = new Date(start_date);
-//   const endDate = end_date ? new Date(end_date) : new Date();
-
-//   const durationYears = differenceInYears(endDate, startDate);
-//   const durationMonths = differenceInMonths(endDate, startDate) % 12;
-
-//   let durationText = '';
-//   if (durationYears > 0) {
-//     durationText += `${durationYears} Year${durationYears > 1 ? 's' : ''} `;
-//   }
-//   if (durationMonths > 0 || durationYears === 0) {
-//     durationText += `${durationMonths} Month${durationMonths > 1 ? 's' : ''}`;
-//   }
-
-//   return (
-//     <Card className='flex items-center gap-5 py-4 px-6 border border-neutral-300 dark:border-neutral-900'>
-//       {logo ? (
-//         <Image src={logo} width={55} height={55} alt={company} />
-//       ) : (
-//         <CompanyIcon size={50} />
-//       )}
-
-//       <div className='space-y-1'>
-//         <h6>{position}</h6>
-//         <div className='text-sm text-neutral-600 dark:text-neutral-400 space-y-2'>
-//           <div className='flex items-center gap-1 md:gap-2'>
-//             <a
-//               href={link || '#'}
-//               target='_blank'
-//               data-umami-event={`Click Career Company Name: ${company}`}
-//             >
-//               <span className='underline cursor-pointer hover:text-dark hover:dark:text-white'>
-//                 {company}
-//               </span>
-//             </a>
-//             <span className='text-neutral-300 dark:text-neutral-700'>•</span>
-//             <span>{location}</span>
-//           </div>
-//           <div className='flex flex-col md:text-[13px]'>
-//             <div className='flex gap-1'>
-//               <span>{format(startDate, 'MMM yyyy')}</span> -{' '}
-//               <span>{end_date ? format(endDate, 'MMM yyyy') : 'Present'}</span>
-//             </div>
-//             <span className='text-neutral-500 dark:text-neutral-500'>
-//               ~ {durationText}
-//             </span>
-//           </div>
-//         </div>
-//       </div>
-//     </Card>
-//   );
-
-import { BsBuildings as CareerIcon } from 'react-icons/bs';
-
-import SectionHeading from '@/common/components/elements/SectionHeading';
-import SectionSubHeading from '@/common/components/elements/SectionSubHeading';
+import Card from '@/common/components/elements/Card';
+import Image from '@/common/components/elements/Image';
 import { CareerProps } from '@/common/types/careers';
 
-import CareerCard from './CareerCard';
+const CareerCard = ({
+  position,
+  company,
+  logo,
+  location,
+  start_date,
+  end_date,
+  link,
+}: CareerProps) => {
+  const startDate = new Date(start_date);
+  const endDate = end_date ? new Date(end_date) : new Date();
 
-const CareerList = ({
-  careerList,
-}: {
-  careerList: CareerProps[];
-}) => {
+  const durationYears = differenceInYears(endDate, startDate);
+  const durationMonths = differenceInMonths(endDate, startDate) % 12;
+
+  let durationText = '';
+  if (durationYears > 0) {
+    durationText += `${durationYears} Year${durationYears > 1 ? 's' : ''} `;
+  }
+  if (durationMonths > 0 || durationYears === 0) {
+    durationText += `${durationMonths} Month${durationMonths > 1 ? 's' : ''}`;
+  }
+
   return (
-    <section className='space-y-6'>
-      <div className='space-y-2'>
-        <SectionHeading
-          title='Career'
-          icon={<CareerIcon size={22} className='mr-1' />}
-        />
-        <SectionSubHeading>
-          <p className='dark:text-neutral-400'>My professional journey.</p>
-        </SectionSubHeading>
-      </div>
+    <Card className='flex items-center gap-5 py-4 px-6 border border-neutral-300 dark:border-neutral-900'>
+      {logo ? (
+        <Image src={logo} width={55} height={55} alt={company} />
+      ) : (
+        <CompanyIcon size={50} />
+      )}
 
-      <div className='grid md:grid-cols-1 gap-4'>
-        {careerList?.map((item, index) => (
-          <CareerCard key={index} {...item} />
-        ))}
+      <div className='space-y-1'>
+        <h6>{position}</h6>
+        <div className='text-sm text-neutral-600 dark:text-neutral-400 space-y-2'>
+          <div className='flex items-center gap-1 md:gap-2'>
+            <a
+              href={link || '#'}
+              target='_blank'
+              data-umami-event={`Click Career Company Name: ${company}`}
+            >
+              <span className='underline cursor-pointer hover:text-dark hover:dark:text-white'>
+                {company}
+              </span>
+            </a>
+            <span className='text-neutral-300 dark:text-neutral-700'>•</span>
+            <span>{location}</span>
+          </div>
+          <div className='flex flex-col md:text-[13px]'>
+            <div className='flex gap-1'>
+              <span>{format(startDate, 'MMM yyyy')}</span> -{' '}
+              <span>{end_date ? format(endDate, 'MMM yyyy') : 'Present'}</span>
+            </div>
+            <span className='text-neutral-500 dark:text-neutral-500'>
+              ~ {durationText}
+            </span>
+          </div>
+        </div>
       </div>
-    </section>
+    </Card>
   );
-};
-
-export default CareerList;
-// };
-
-// export default CareerCard;
