31c5d981e87ff4fbd0253833f81bb9f32ba16b93
mpc <mpengchong@gmail.com>
2024-12-08 10:19:12 -0600

    Update CareerCard.tsx

--- src/modules/about/components/CareerCard.tsx
+++ src/modules/about/components/CareerCard.tsx
@@ -26,16 +26,17 @@ const CareerCard = ({
       )}
 
       <div className='space-y-1'>
-        <a
-          href={link || '#'}
-          target='_blank'
-          data-umami-event={`Click Career Company Name: ${company}`}
-        >
-          <h6>{position}</h6>
-        </a>
+        <h6>{position}</h6>
         <div className='space-y-2 text-sm text-neutral-600 dark:text-neutral-400'>
           <div className='flex flex-col gap-1 md:flex-row md:gap-2'>
-            <span>{company}</span>
+            <a
+              href={link || '#'}
+              target='_blank'
+              data-umami-event={`Click Career Company Name: ${company}`}
+              className='underline cursor-pointer hover:text-dark hover:dark:text-white'
+            >
+              <span>{company}</span>
+            </a>
             <span className='hidden text-neutral-300 dark:text-neutral-700 md:flex'>
               •
             </span>
