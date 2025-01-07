84918e0cafb62ea920fbda173656158b5b15caf4
Pengchong Ma <mpengchong@gmail.com>
2025-01-07 15:25:59 -0600

    Update siteMetadata.ts

--- src/contents/siteMetadata.ts
+++ src/contents/siteMetadata.ts
@@ -66,7 +66,7 @@ export const author = {
   status: 'Hybrite',
   workingOn: `I am exploring the intersection of bioinformatics and AI, focusing on cutting edge data analysis and visualization techniques.`,
   workTogether: `I’m open to new collaborations, especially in bioinformatics single cell. Feel free to Email me to discuss potential opportunities.`,
-  wakatime: 'https://wakatime.com/',
+  wakatime: 'https://wakatime.com/@PengchongMa',
   github_accounts: [
     {
       username: 'mpckkk',
