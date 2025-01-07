147ce2e8f18ff314f340c68fc4c5aa71ee9faba8
Pengchong Ma <mpengchong@gmail.com>
2025-01-07 15:20:02 -0600

    Update siteMetadata.ts

--- src/contents/siteMetadata.ts
+++ src/contents/siteMetadata.ts
@@ -7,7 +7,7 @@ export const siteMetadata = {
   siteScreenshot: '',
   siteDescription:
     'Self-motivated and dedicated developer with research passion in bioinformatics and data science. Leveraging over 5 years of experience in tech, I create intuitive and high performance web applications with a focus on plant single cell.',
-  author: 'PengchongMa',
+  author: 'Pengchong Ma',
   fullName: 'Pengchong Ma',
   location: 'Lincoln, NE, USA',
   timeZone: 'America/Chicago',
@@ -57,7 +57,7 @@ export const socialAccounts = {
 };
 
 export const author = {
-  name: 'Pengchong Ma',
+  name: 'PengchongMa',
   location: 'Lincoln, NE, USA',
   timeZone: 'America/Chicago',
   locationIcon: '🇺🇸',
