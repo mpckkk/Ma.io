b138af054e817c6e14afe140ac5ac81a1f64a476
Pengchong Ma <mpengchong@gmail.com>
2026-04-21 01:20:18 -0500

    Update author's location from 'American' to 'Lincoln'

--- src/contents/siteMetadata.ts
+++ src/contents/siteMetadata.ts
@@ -58,7 +58,7 @@ export const socialAccounts = {
 
 export const author = {
   name: 'PengchongMa',
-  location: 'American',
+  location: 'Lincoln',
   timeZone: 'American/Chicago',
   locationIcon: '',
   position: 'Research Assistant',
