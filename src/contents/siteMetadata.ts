08ba715a84d3d16156dbae87828a356fbec69491
Pengchong Ma <mpengchong@gmail.com>
2026-04-21 01:25:13 -0500

    Update status field in siteMetadata

--- src/contents/siteMetadata.ts
+++ src/contents/siteMetadata.ts
@@ -61,7 +61,7 @@ export const author = {
   location: 'Lincoln',
   timeZone: 'American/Chicago',
   locationIcon: '',
-  status:'',
+  status:'-.- +.+ =.= @.@',
   position: 'Research Assistant',
   calMeetingUrl: '',
   workingOn: `I am exploring the intersection of bioinformatics and AI.`,
