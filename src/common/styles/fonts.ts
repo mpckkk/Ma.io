b9f08cf7c955a826241151ed6acb8a4af059d242
Pengchong Ma <mpengchong@gmail.com>
2026-07-07 10:57:59 -0500

    Add fontFamily styles to font constants

--- src/common/styles/fonts.ts
+++ src/common/styles/fonts.ts
@@ -1,4 +1,4 @@
-export const jakartaSans = { variable: '', className: '', style: {} };
-export const firaCode = { variable: '', className: '', style: {} };
-export const soraSans = { variable: '', className: '', style: {} };
-export const onestSans = { variable: '', className: '', style: {} };
+export const jakartaSans = { variable: '', className: '', style: { fontFamily: 'sans-serif' } };
+export const firaCode = { variable: '', className: '', style: { fontFamily: 'monospace' } };
+export const soraSans = { variable: '', className: '', style: { fontFamily: 'sans-serif' } };
+export const onestSans = { variable: '', className: '', style: { fontFamily: 'sans-serif' } };
