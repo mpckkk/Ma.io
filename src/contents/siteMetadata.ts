6430bbf40b6deab55b1edee71da3227ec36f2162
Pengchong Ma <mpengchong@gmail.com>
2026-07-07 10:35:02 -0500

    Update siteMetadata

--- src/contents/siteMetadata.ts
+++ src/contents/siteMetadata.ts
@@ -65,7 +65,7 @@ export const author = {
   position: 'Research Assistant',
   calMeetingUrl: '',
   workingOn: `I am exploring the intersection of bioinformatics and AI.`,
-  workTogether: `I’m open to new collaborations, especially in bioinformatics single cell. Feel free to Email me to discuss potential opportunities.`,
+  workTogether: `I’m open to new collaborations, especially in bioinformatics. Feel free to Email me to discuss potential opportunities.`,
   wakatime: 'https://wakatime.com/@PengchongMa',
   github_accounts: [
     {
