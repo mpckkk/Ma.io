59087ac67179938063684c3b5b2d1be8357d770b
Pengchong Ma <mpengchong@gmail.com>
2026-09-01 17:11:24 -0500

    Remove status field from siteMetadata
    Removed the status field from site metadata.

--- src/contents/siteMetadata.ts
+++ src/contents/siteMetadata.ts
@@ -61,7 +61,6 @@ export const author = {
   location: 'Lincoln',
   timeZone: 'American/Chicago',
   locationIcon: '',
-  status:'-.- +.+ =.= @.@',
   position: 'Research Assistant',
   calMeetingUrl: '',
   workingOn: `I am exploring the intersection of bioinformatics and AI.`,
